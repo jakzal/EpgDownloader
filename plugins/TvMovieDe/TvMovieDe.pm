7d8f754e55100aa143e4969fa48ce5c66c616aca
Jakub Zalas <jakub@zalas.pl>
2006-05-01 13:10:04 +0000

    Fixed TvMovieDe import plugin
    git-svn-id: https://epgdownloader.svn.sourceforge.net/svnroot/epgdownloader/trunk/epgdownloader@28 828d0981-3b27-4f87-91c1-ce48478c34ae

--- plugins/TvMovieDe/TvMovieDe.pm
+++ plugins/TvMovieDe/TvMovieDe.pm
@@ -1,6 +1,7 @@
 package TvMovieDe;
 use constant PLUGIN_NAME => TvMovieDe;
 use constant BROWSER => 'Opera/7.54 (X11; Linux i686; U)';
+use constant FORM_NUMBER => 1;
 use WWW::Mechanize;
 use Date::Format;
 use Date::Parse;
@@ -23,7 +24,7 @@ This software is released under the GNU GPL version 2.
 
 Author: Jakub Zalas <jakub@zalas.net>.
 
-Date: march, april 2006
+Date: march, april, may 2006
 
 =cut
 
@@ -36,7 +37,7 @@ sub new {
 	$self->{'config'} = $config;
 	$self->{'plugin_config'} = ConfigTvMovieDe->new('config.xml');
 	
-	$self->{'url'} = 'http://www.tvmovie.de/suche/profisuche.html';
+	$self->{'url'} = 'http://tvmovie.de/TV_nach_Sendern.22.0.html';
 	
 	bless( $self, $class );
 	return $self;
@@ -50,105 +51,81 @@ sub get {
 	my $events = ();
 	my $url = $self->{'url'};
 
+	my $browser = WWW::Mechanize->new( 'agent' => BROWSER );
+		
+	$browser->get($url);
+	
+	my $channelsList = $browser->content();
+	
+	if($channelsList !~ s/(.*)<select name="senderid\[\]"(.*?)>(.*?)<\/select>(.*)/$3/sm) {
+		Misc::pluginMessage(PLUGIN_NAME, "ERROR: Couldn't parse website!");
+		return $channels;
+	}
+	
 	foreach my $name (keys(%{$channels})) {
 		Misc::pluginMessage(PLUGIN_NAME,"Downloading schedule for ".$name," ");
 		
 		my $events = $channels->{$name};
 		
-		my $browser = WWW::Mechanize->new( 'agent' => BROWSER );
+		my $channelId = $channelsList;
 		
-		$browser->get($url);
-		
-		my $content = $browser->content();
-		$content =~ s/(.*)<b><br>Sender<\/b>(.*)/$2/sm;
-	
-		#check if channel exists
-		if($content !~ s/(.*)<input type="checkbox" name="sender" value="(.*?)">(&nbsp;)$name<\/font><\/td>(.*)//sm) {
-			Misc::pluginMessage("","");
-				Misc::pluginMessage(
-					PLUGIN_NAME,
-					"ERROR: Schedule for channel '$name' not found!");
+		if($channelId !~ s/(.*)<option value="(.*?)">$name<\/option>(.*)/$2/sm) {
+			Misc::pluginMessage(PLUGIN_NAME, "ERROR: Schedule for channel '$name' not found!");
 			next;
 		}
-	
-		#channel checkbox
-		my $channelId = $2;
-		$browser->form_name("profisucheFinderForm");
-		$browser->tick("sender", $channelId);
 		
-		#hour checkboxes
-		for(my $i=0; $i<24; $i++) {
-			my $value = $i;
-			$value = "0".$value if $value =~ /^[0-9]$/;
-			$browser->tick("zeiten", $value);
-		}
+		my $base_uri = $url."?senderid[]=".$channelId;
 		
-		#get schedule for each day, couldn't get all at one time because website has restrictions to 100 rows
-		my @germanDays = ( 'sa', 'so', 'mo', 'di', 'mi', 'do', 'fr' );
-		my ($second, $minute, $hour, $day, $month, $year, $weekDay, $dayOfYear, $isDST) = localtime(time()+(60*60*24));
-		my $days = $self->{'plugin_config'}->get('DAYS');
-		my $d = $weekDay;
-		my $base_uri = "";
-		
-		for(my $i=0; $i < $days; $i++) {
+		for(my $i=0; $i < $self->{'plugin_config'}->{'DAYS'}; $i++) {
 			my $dateString = time2str("%Y-%m-%d",time+(60*60*24*($i)));
-			$d = $d+$i;
-			$d = 0 if !exists($germanDays[$d]);
 			
-			#first day is form submittet, the rest is get from uri
-			if($i==0) {
-				$browser->tick("tage", $germanDays[$d]);
-				$browser->submit();
-				$base_uri = $browser->uri();
-			} else {
-				my $current_uri = $base_uri;
-				$current_uri =~ s/(.*?)(tage=)([smdf][aoir])(.*)/$1$2$germanDays[$d]$4/;
-				$browser->get($current_uri);
-			}
+			$browser->get($base_uri."&date=".$dateString);
 		
-			$content = $browser->content();
-			$content =~ s/(.*?)<a name="suchergebnisse" \/>(\s)+<table(.*?)>(.*?)<table(.*?)>(.*?)<\/table>(.*)/$6/sm;
-		
+			my $content = $browser->content();
+ 			$content =~ s/(.*?)Haupttabelle Anfang(.*?)Haupttabelle Ende(.*)/$2/sm;
+			
 			#parse schedule
-			while($content =~ s/(.*?)<tr>(.*?)<\/tr>(.*)/$3/sm) {
-				my $row = $2;
+			while($content =~ s/(.*?)<tr id="l(.*?)">(.*?)<\/tr>(.*)/$4/sm) {
+				my $row = $3;
 			
-				my $colReg = "(.*?)<td(.*?)>(.*?)<\/td>";
-			
-				next if $row !~ s/$colReg$colReg$colReg$colReg$colReg$colReg/$3/sm;
-			
-				my $hour = $6;
-				my $title = $15;
-				my $description = "";
-				my $category = $18;
-			
-				$title =~ s/(.*?)<b>(.*?)<\/b>(.*)/$2/sm;
-				$description = $3;
-
-				#remove trash
-				$hour =~ s/<(.*?)>//smg;
-				$title =~ s/<(.*?)>//smg;
-				$description =~ s/<(.*?)>//smg;
-				$category =~ s/<(.*?)>//smg;
-
-				#convert hour to unix timestamp, if it's after midnight, change base date string
-				$dateString = time2str("%Y-%m-%d",time+(60*60*24*($i))) if $hour =~ /0[0-3]{1}:[0-9]{2}/;
-				$hour = str2time($dateString." ".$hour);
-
-				#create event
-				my $event = Event->new();
-				$event->set('start',$hour);
-				$event->set('stop',$hour+1);
-				$event->set('title',$title);
-				$event->set('description',$description);
-				$event->set('category',$category);
-	
-				#set the previous event stop timestamp
-				my $previous = $#{$events};
-				$events->[$previous]->set('stop',$event->{'start'}) if $previous > -1;
+				next if $row !~ s/(.*?)<td(.*?)><\/td>(.*?)<td(.*?)>(.*?)<\/td>(.*)/$5/sm;
 				
-				#put event to the events array
-				push @{$events}, $event;
+				while($row =~ s/(.*?)<span(.*?)>(.*?)<\/span>(.*?)<a(.*?)>(.*?)<\/a>(.*?)<br \/>(.*)/$8/sm) {
+					my $hour = $3;
+					my $title = $6;
+					my $description = $7;
+					my $category = $7;
+					
+					$description = "" if($description !~ s/(.*?)\((.*?)\)(.*)/$2/);
+					$category = "" if($category !~ s/(.*?\))(.*?)([-0-9]{1,})(.*)/$2/);
+					
+					$description =~ s/^\s//; $description =~ s/\s$//;
+					$category =~ s/^\s//; $category =~ s/\s$//;
+					
+					#'&' are already in proper format
+					$title =~ s/&amp;/&/g;
+					$description =~ s/&amp;/&/g;
+					$category =~ s/&amp;/&/g;
+					
+					#convert hour to unix timestamp, if it's after midnight, change base date string
+					$dateString = time2str("%Y-%m-%d",time+(60*60*24*($i))) if $hour =~ /0[0-3]{1}:[0-9]{2}/;
+					$hour = str2time($dateString." ".$hour);
+
+					#create event
+					my $event = Event->new();
+					$event->set('start',$hour);
+					$event->set('stop',$hour+1);
+					$event->set('title',$title);
+					$event->set('description',$description);
+					$event->set('category',$category);
+	
+					#set the previous event stop timestamp
+					my $previous = $#{$events};
+					$events->[$previous]->set('stop',$event->{'start'}) if $previous > -1;
+					
+					#put event to the events array
+					push @{$events}, $event;
+				}
 				
 			}
 
