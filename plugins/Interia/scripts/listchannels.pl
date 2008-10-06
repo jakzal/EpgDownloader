c88ea1bd7ef9ba66c0cfae36f1aaa4d22b17ef89
Jakub Zalas <jakub@zalas.pl>
2008-10-06 21:21:19 +0000

    Fixed available channels list generation for Interia nad regenerated xml.
    git-svn-id: https://epgdownloader.svn.sourceforge.net/svnroot/epgdownloader/trunk/epgdownloader@36 828d0981-3b27-4f87-91c1-ce48478c34ae

--- plugins/Interia/scripts/listchannels.pl
+++ plugins/Interia/scripts/listchannels.pl
@@ -4,7 +4,8 @@
 use constant OUTPUT_FILE => "../channels.xml";
 use constant PLUGIN_NAME => Interia;
 use constant BROWSER => 'Opera/7.54 (X11; Linux i686; U)';
-use constant LIST_URL => "http://program.interia.pl";
+use constant LIST_URL => "http://programtv.interia.pl/kanaly/wszystkie,10";
+use constant BASE_URL => "http://programtv.interia.pl/kanaly/";
 
 #include
 use WWW::Mechanize;
@@ -22,7 +23,7 @@ listchannels.pl - Lists channels available in Interia Plugin
 
 =head1 DESCRIPTION
 
-Script connects to http://tv.interia.pl website, checks which channels are available and saves it to file. Configuration is available by editing 'use constant' directives at the beginning of file.
+Script connects to http://programtv.interia.pl website, checks which channels are available and saves it to file. Configuration is available by editing 'use constant' directives at the beginning of file.
 
 =head1 COPYRIGHT
 
@@ -38,14 +39,11 @@ Date: may 2006
 #main
 
 my $browser = WWW::Mechanize->new( 'agent' => BROWSER );
-	
 $browser->get(LIST_URL);
-
-$browser->submit_form(form_name => "nawigacja", fields => {'p' => "Wszystkie"});
 
 my $content = $browser->content();
 
-if($content !~ s/(.*?)<div(.*?)><b>Stacje:<\/b>(.*?)<\/div>(.*)/$3/sm) {
+if($content !~ s/(.*?)<ul id="channelList">(.*?)<\/ul>(.*)/$2/sm) {
 	print "Unable to find channels list.\n";
 	exit;
 }
@@ -54,9 +52,11 @@ open(FILE,">".OUTPUT_FILE);
 
 print FILE "<CHANNELS>\n";
 
-while($content =~ s/(.*?)<a class=stacja(.*?)href=\"(.*?)\">(.*?)<\/a>(.*)/$5/sm) {
-	my $url = LIST_URL.$3;
-	my $channel = $4;
+#<a href="wszystkie/tvp-1,44,10?sDate=2008-10-06&sTimeF=22&sTimeT=4&sTimeT=4&cn=&p=1">TVP 1</a>
+
+while($content =~ s/(.*?)<a href=\"(.*?)\">(.*?)<\/a>(.*)/$4/sm) {
+	my $url = BASE_URL.$2;
+	my $channel = $3;
 	
 	$channel =~ s/^[\s]//;
 	$channel =~ s/[\s]$//;
