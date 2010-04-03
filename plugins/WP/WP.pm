85cb0ff3e61ba3d74671baf972d194f7e3a14a16
Jakub Zalas <jakub@zalas.pl>
2010-04-03 21:23:11 +0200

    Fixed WP plugin issues caused by changed website structure.

--- plugins/WP/WP.pm
+++ plugins/WP/WP.pm
@@ -1,6 +1,7 @@
 package WP;
 use constant PLUGIN_NAME => WP;
 use constant BROWSER => 'Opera/7.54 (X11; Linux i686; U)';
+use Encode;
 use WWW::Mechanize;
 use Date::Format;
 use Date::Parse;
@@ -36,7 +37,7 @@ sub new {
 	$self->{'config'} = $config;
 	$self->{'plugin_config'} = ConfigWP->new('config.xml');
 	
-	$self->{'url'} = 'http://tv.wp.pl/katn,Lista kana��w,programy.html';
+	$self->{'url'} = 'http://tv.wp.pl';
 	
 	bless( $self, $class );
 	return $self;
@@ -48,18 +49,18 @@ sub get {
 	my $channels = shift;
 	
 	my $url = $self->{'url'};
-	
-	my $days 		= $self->{'plugin_config'}->get('DAYS');
-	my $fullDescription 	= $self->{'plugin_config'}->get('FULL_DESCRIPTION');
+	my $days = $self->{'plugin_config'}->get('DAYS');
+	my $fullDescription = $self->{'plugin_config'}->get('FULL_DESCRIPTION');
+	my $browser = WWW::Mechanize->new( 'agent' => BROWSER );
+
+	$browser->get($url);
+
+	my $list_content = $browser->content();
 	
 	foreach my $name (keys(%{$channels})) {
 		Misc::pluginMessage(PLUGIN_NAME,"Downloading schedule for ".$name," ");
 		
 		my $events = $channels->{$name};
-	
-		my $browser = WWW::Mechanize->new( 'agent' => BROWSER );
-	
-		$browser->get($url);
 	
 		#special treatment for '+', '(', ')'
 		$name =~ s/\+/\\\+/g;
@@ -67,75 +68,71 @@ sub get {
 		$name =~ s/\)/\\\)/g;
 
     my $channelRegex = qr/$name$/;
-    
-    if (!$browser->find_link(text_regex => $channelRegex)) {
+
+    $list_content =~ /.*?<option (value|id)="([^"]+)" (value|id)="([^"]+)">$name<\/option>(.*)/sm;
+
+	  if (("$1" ne 'id' and "$3" ne 'id') or ("$1" ne 'value' and "$3" ne 'value')) {
       Misc::pluginMessage(PLUGIN_NAME,"Could not find schedule for ".$name);
       next;
     }
-	
-		$browser->follow_link(text_regex => $channelRegex);
-	
+
+		my $id = ("$1" eq 'id' ? $2 : $4);
+		my $value = ("$3" eq 'value' ? $4 : $2);
+		my $channel_uri = $url.'/name,'.$id.',stid,'.$value.',time,0,program.html';
+
 		#special treatment for '+', '(', ')'
 		$name =~ s/\\\+/+/g;
 		$name =~ s/\\\(/\(/g;
 		$name =~ s/\\\)/\)/g;
-		
-		my $base_uri = $browser->uri();
 		
 		for(my $i=1; $i <= $days; $i++) {
 		
 			my $dateString = time2str("%Y-%m-%d",time+(60*60*24*($i-1)));
 			my $dateUnix = str2time($dateString);
 			my $date_url = "";
+
+			$channel_uri =~ s/name,/date,$dateString,name,/;
 			
-			$browser->get($base_uri."&T[date]=".$dateString."&T[time]=0") if $i>1;
+			$browser->get($channel_uri);
 		
       #@todo From version 1.50 of WWW-Mechanize content is decoded by default. For now we have to handle it this way.
 			#my $content = $browser->content();
       my $content = $browser->response()->decoded_content();
-			if($content !~ s/(.*)<table(.*?)>(.*?)Program na(.*?)<\/table>(.*)/$4/sm) {
+			$content = encode('utf8', $content);
+			if($content !~ /(.*)<div class="program">(.*)/sm) {
 				Misc::pluginMessage("","");
 				Misc::pluginMessage(
 					PLUGIN_NAME,
 					"ERROR: Schedule for channel '$name' on '$dateString' not found!"," ");
 				last;
 			}
-			$content =~ s/(.*?)<\/div>(.*)/$2/sm;
-	
-			while($content =~ s/(.*?)<tr>(.*?)<\/tr>(.*)/$3/sm) {
-				my $row = $2;
-	
-				my $hour = $row;
-				last if $hour !~ s/(.*?)<b>([0-9]{1,2}:[0-9]{2})<\/b>(.*)/$2/sm;
-				
-				my $title = $3;
-				$title =~ s/(.*?)<b>(.*?)<\/b>(.*)/$2/sm;
-		
-				my $description = $3;
-				my $description2 = "";
-				
-        $description =~ s/(.*?)<span(.*?)>(.*?)<\/span>(.*)/$3/sm;
-        $description =~ s/<a href="javascript:okno.*?".*?>.*?<\/a>//smg;
-        $description =~ s/(&nbsp;|&raquo;)//smg;
+
+			while($content =~ s/.*?<div class="program">.*?<div class="programL">.*?<strong>(.*?)<\/strong>.*?<span>\((.*?)\)<\/span>.*?<div class="programR">.*?<h4><a title="(.*?)" href="(.*?)" onclick="return opis\('(.*?)', .*?\);">(.*?)<\/a>.*?<\/h4>.*?<p class="opis">(.*?)<\/p>.*?<p class="ekipa">(.*?)<\/p>(.*)/$9/sm) {
+				my $hour    = $1;
+				my $length  = $2;
+				my $title   = $3;
+				my $longUrl = $5;
+				my $description  = $length.", ".$7;
+				my $description2 = $8;
+
+				last if $hour !~ /([0-9]{1,2}:[0-9]{2})/;
 				
 				#get full description if available and needed (follows another link so it costs time)
-				if($fullDescription == 1 && $title =~ /(.*?)javascript:okno\(\'(.*?)\'(.*)/) {
-					$browser->get($2);
+				if($fullDescription == 1 && $longUrl !~ //) {
+					$browser->get($longUrl);
           #@todo From version 1.50 of WWW-Mechanize content is decoded by default. For now we have to handle it this way.
 					#my $tmp = $browser->content();
           my $tmp = $browser->response()->decoded_content();
-					$description  = $1 if $tmp =~ /.*?<p class="op">(.*?)<\/p>.*/sm;
-					$description2 = $1 if $tmp =~ /.*?<p class="wystepuja">(.*?)<\/p>.*/sm;
-          $description2.= $1 if $tmp =~ /.*?<span class="czas">(.*?)<\/span>.*/sm;
+					$description  = $1.", ".$2 if $tmp =~ /.*?<div class="opis">(.*?)<\/div>.*?<p>(.*?)<\/p>/sm;
+					$description2 = $1 if $tmp =~ /.*?<p class="ekipa">(.*?)<\/p>.*/sm;
 				}
 				
-				#remove html tags from title
-				$title =~ s/<(\/?)(.*?)>//smg;
-		
 				#removing trash from description
+				$description  =~ s/&nbsp;/ /smg;
 				$description  =~ s/<br(.*?)>/\n/smgi;
 				$description  =~ s/<(\/?)(.*?)>//smg;
         $description  =~ s/\s+/ /g;
+				$description2 =~ s/&nbsp;/ /smg;
 				$description2 =~ s/<br(.*?)>/\n/smgi;
 				$description2 =~ s/<(\/?)(.*?)>//smg;
         $description2 =~ s/\s+/ /g;
